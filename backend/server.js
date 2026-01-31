cb8fcd9453c896c949d9d77a3c00d0fb50ae2a73
pavanbm-max <pavan.b.m1598@gmail.com>
2026-01-31 17:22:21 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,9 @@ app.get("/", (req, res) => {
   res.send("API running");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
